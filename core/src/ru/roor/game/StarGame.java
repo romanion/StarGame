643ae327ceab4f6e984d524470330426eb12c7b0
Roman <roman.orlov@phystech.edu>
2020-04-25 19:21:46 +0300

    homework 1

--- core/src/ru/roor/game/StarGame.java
+++ core/src/ru/roor/game/StarGame.java
@@ -13,7 +13,7 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("space.jpg");
 	}
 
 	@Override
