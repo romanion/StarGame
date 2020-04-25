ca41603325b4139643003a1297b19d24c6df5ca1
Roman Orlov <Roman Orlov>
2020-04-25 18:59:07 +0300

    Initial commit

---
+++ desktop/src/ru/roor/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.roor.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.roor.game.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
