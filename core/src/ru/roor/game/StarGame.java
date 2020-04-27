4fed1f0dd0568d5d0a9c79dac1a84f7980fa5b2a
Roman <roman.orlov@phystech.edu>
2020-04-27 22:37:27 +0300

    homework 2

--- core/src/ru/roor/game/StarGame.java
+++ core/src/ru/roor/game/StarGame.java
@@ -1,33 +1,13 @@
 package ru.roor.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import ru.roor.game.screen.MenuScreen;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class StarGame extends Game {
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("space.jpg");
+	setScreen(new MenuScreen());
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
