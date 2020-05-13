505e602335887da7ae7df10f1e1f7047304cd030
Roman <roman.orlov@phystech.edu>
2020-05-14 01:52:54 +0300

    Pre-hw4

--- core/src/ru/roor/game/sprite/MainShip.java
+++ core/src/ru/roor/game/sprite/MainShip.java
@@ -4,10 +4,8 @@ package ru.roor.game.sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-
 import ru.roor.game.base.BaseShip;
 import ru.roor.game.math.Rect;
-import sun.tools.jar.Main;
 
 public class MainShip extends BaseShip {
 
@@ -18,9 +16,6 @@ public class MainShip extends BaseShip {
     private static final float L_SPEED = 0.01f;
     private Vector2 speed;
     private Vector2 auxiliary;
-    private Vector2 auxiliary1;
-    private Vector2 norm1;
-    private Vector2 norm2;
 
     public MainShip(TextureRegion [][] textureRegions) {
         super(textureRegions[0][0]);
@@ -29,9 +24,6 @@ public class MainShip extends BaseShip {
 
         speed = new Vector2();
         auxiliary = new Vector2();
-        auxiliary1 = new Vector2();
-        norm1 = new Vector2(1, 0);
-        norm2 = new Vector2(-1, 0);
 
     }
 
@@ -57,18 +49,17 @@ public class MainShip extends BaseShip {
     @Override
     public void update(float delta) {
         if (touch != null && !isDestinationReached(touch)) {
-            speed.set(auxiliary).setLength(L_SPEED);
+            speed.set(auxiliary.x, 0).setLength(L_SPEED);
                 pos.add(speed);
         }
     }
 
     public boolean isDestinationReached(Vector2 dest) {
-        auxiliary1.set(dest.x, 0);
         if (isMe(dest)) {
             return true;
         }
-        auxiliary.set(dest.x, 0);
-        return Math.abs(auxiliary.x - pos.x) < 0.015f ? true : false;
+        auxiliary.set(dest);
+        return Math.abs(auxiliary.sub(pos).x) < 0.015f ? true : false;
     }
 
 
